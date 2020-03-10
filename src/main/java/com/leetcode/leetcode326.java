0503bed6025aa2485c01ff032d18d8616fe3393e
zmh <1656276501@qq.com>
2020-03-11 02:21:43 +0800

    leetcode

---
+++ src/main/java/com/leetcode/leetcode326.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+public class leetcode326 {
+    public boolean isPowerOfThree(int n) {
+        while(n>1){
+            if(n%3!=0){
+                return false;
+            }
+            n/=3;
+        }
+        return n==1;
+    }
+}
--- src/main/java/com/leetcode/leetcode367.java
+++ src/main/java/com/leetcode/leetcode367.java
@@ -2,48 +2,48 @@ package com.leetcode;
 
 /**
  * 给定一个正整数 num，编写一个函数，如果 num 是一个完全平方数，则返回 True，否则返回 False。
- *
+ * <p>
  * 说明：不要使用任何内置的库函数，如  sqrt。
- *
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：16
  * 输出：True
  * 示例 2：
- *
+ * <p>
  * 输入：14
  * 输出：False
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/valid-perfect-square
  */
 public class leetcode367 {
 
     public boolean isPerfectSquare(int num) {
-      int left = 1, right = num;
+        int left = 1, right = num;
 
-      while (left<right){
-          int mid = left +(right-left)/2;
-          if (num%mid==0&& num/mid == mid){
-              return true;
-          }else if (num/mid > mid){
-              left = mid+1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (num % mid == 0 && num / mid == mid) {
+                return true;
+            } else if (num / mid > mid) {
+                left = mid + 1;
 
-          }else {
-              right = mid-1;
-          }
-      }
-      return num%left==0&& num/left == left;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
 
     }
+
     public boolean isPerfectSquare1(int num) {
         int temp = 1;
         int k = 3;
-        while (temp<num){
-            temp+=k;
-            k+=2;
+        while (temp < num) {
+            temp += k;
+            k += 2;
         }
-        return temp==num;
-
+        return temp == num;
     }
 }

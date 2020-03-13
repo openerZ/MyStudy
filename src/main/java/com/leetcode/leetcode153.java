97e92910a01e917cf69c19a285441b0038d4cb71
zmh <1656276501@qq.com>
2020-03-14 02:03:04 +0800

    leetcode

---
+++ src/main/java/com/leetcode/leetcode153.java
@@ -0,0 +1,37 @@
+package com.leetcode;
+
+/**
+ * 153. 寻找旋转排序数组中的最小值
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ *
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ *
+ * 请找出其中最小的元素。
+ *
+ * 你可以假设数组中不存在重复元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,4,5,1,2]
+ * 输出: 1
+ * 示例 2:
+ *
+ * 输入: [4,5,6,7,0,1,2]
+ * 输出: 0
+ */
+public class leetcode153 {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        int l =0,r=n-1;
+        while (l<r){
+            int mid = l+(r-l)/2;
+            if (nums[mid]<nums[r]){
+                r=mid;
+            }else {
+                l=mid+1;
+            }
+        }
+
+        return nums[l];
+    }
+}

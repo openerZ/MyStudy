967ac6bc233096b0696e8c8fd9dc9fee73d21a79
zmh <1656276501@qq.com>
2020-03-21 15:02:54 +0800

    leetcode

--- src/main/java/com/leetcode/leetcode343.java
+++ src/main/java/com/leetcode/leetcode343.java
@@ -17,6 +17,21 @@ package com.leetcode;
  */
 public class leetcode343 {
 
+
+    public int integerBreak1(int n) {
+        if (n<4) return n-1;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for (int i = 4; i <=n ; i++) {
+            for (int j = 1; j <=i/2 ; j++) {
+                dp[i] = Math.max(dp[i],dp[j]*dp[i-j]);
+            }
+        }
+        return dp[n];
+
+    }
     public int integerBreak(int n) {
         if (n<4) return n-1;
         int a = n/3;
